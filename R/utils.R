fce45fdbda342782187d098da5367c19b1e31ef2
Simon Adamov <simon.adamov@mailbox.org>
2021-02-09 14:38:20 +0100

    Added Municipality Plot

--- R/utils.R
+++ R/utils.R
@@ -228,3 +228,74 @@ impute_daily <- function(data) {
       value = if_else(is.na(value), 0, value)
     )
 }
+
+#' Define a customized theme for ggplot
+#'
+
+my_maptheme <- function(...) {
+  theme_minimal() +
+    theme(
+      text = element_text(
+        family = "Verdana",
+        color = "#22211d"
+      ),
+      # remove all axes
+      axis.line = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank(),
+      # add a subtle grid
+      panel.grid.major = element_line(color = "#dbdbd9", size = 0.2),
+      panel.grid.minor = element_blank(),
+      # background colors
+      plot.background = element_rect(
+        fill = "#f5f5f2",
+        color = NA
+      ),
+      panel.background = element_rect(
+        fill = "#f5f5f2",
+        color = NA
+      ),
+      legend.background = element_rect(
+        fill = "#f5f5f2",
+        color = NA
+      ),
+      # borders and margins
+      plot.margin = unit(c(.5, .5, .2, .5), "cm"),
+      panel.border = element_blank(),
+      panel.spacing = unit(c(-.1, 0.2, .2, 0.2), "cm"),
+      # titles
+      legend.title = element_text(size = 11),
+      legend.text = element_text(
+        size = 9, hjust = 0,
+        color = "#22211d"
+      ),
+      plot.title = element_text(
+        size = 15, hjust = 0.5,
+        color = "#22211d"
+      ),
+      plot.subtitle = element_text(
+        size = 10, hjust = 0.5,
+        color = "#22211d",
+        margin = margin(
+          b = -0.1,
+          t = -0.1,
+          l = 2,
+          unit = "cm"
+        ),
+        debug = F
+      ),
+      # captions
+      plot.caption = element_text(
+        size = 7,
+        hjust = .5,
+        margin = margin(
+          t = 0.2,
+          b = 0,
+          unit = "cm"
+        ),
+        color = "#939184"
+      ),
+      ...
+    )
+}
